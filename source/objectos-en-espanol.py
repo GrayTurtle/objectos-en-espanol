5673d2acfc03f5acf473b8055e5ba3134c82fc41
John Tran <John.Tran@ou.edu>
2018-09-10 15:05:56 -0500

    edit code

--- source/objectos-en-espanol.py
+++ source/objectos-en-espanol.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 
-# importing the requests library
+#curl "https://www.googleapis.com/language/translate/v2?key=AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo&source=en&target=es&q=Hello%20World"
+
 import requests
 
-# defining the api-endpoint
-API_ENDPOINT = "https://www.googleapis.com/language/translate/v2"
-
-# your API key here
+API_VISION = "https://vision.googleapis.com/v1/images:annotate"
+API_TRANSLATE = "https://www.googleapis.com/language/translate/v2"
 API_KEY = "AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo"
 
-# sending post request and saving response as response object
-r = requests.post(url=API_ENDPOINT, data={'key': API_KEY, 'source': 'en', 'target': 'es', 'q': 'Hello World'})
+translate_request = requests.post(url=API_TRANSLATE, data={'key': API_KEY, 'source': 'en', 'target': 'es', 'q': 'Hello World'})
 
-# extracting response text
-pastebin_url = r.text
-print("The pastebin URL is:%s" % pastebin_url)
-
-print('hello bitches')
-
-#curl "https://www.googleapis.com/language/translate/v2?key=AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo&source=en&target=es&q=Hello%20World"
+print(translate_request)
