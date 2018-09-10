91484b1abf092a4f5df5e9028e056bd87b4e4bc0
John Tran <John.Tran@ou.edu>
2018-09-10 13:26:47 -0500

    add request code

--- source/objectos-en-espanol.py
+++ source/objectos-en-espanol.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
+
+# importing the requests library
+import requests
+
+# defining the api-endpoint
+API_ENDPOINT = "https://www.googleapis.com/language/translate/v2"
+
+# your API key here
+API_KEY = "AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo"
+
+# sending post request and saving response as response object
+r = requests.post(url=API_ENDPOINT, data={'key': API_KEY, 'source': 'en', 'target': 'es', 'q': 'Hello World'})
+
+# extracting response text
+pastebin_url = r.text
+print("The pastebin URL is:%s" % pastebin_url)
+
 print('hello bitches')
 
+#curl "https://www.googleapis.com/language/translate/v2?key=AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo&source=en&target=es&q=Hello%20World"
