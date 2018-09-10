20d868c53eaea6f7f7ed300b7ea6f5614fb625fc
John Tran <John.Tran@ou.edu>
2018-09-10 18:36:06 -0500

    fix bug for vision api

--- source/objectos-en-espanol.py
+++ source/objectos-en-espanol.py
@@ -5,11 +5,13 @@
 
 import requests
 
+API_KEY = "AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo"
 API_VISION = "https://vision.googleapis.com/v1/images:annotate"
 API_TRANSLATE = "https://www.googleapis.com/language/translate/v2"
-API_KEY = "AIzaSyDzvkjS7zn7WqucXeJ5_yoARbZj4afSXVo"
 
-vision_request = requests.post(url=API_VISION, data={'key': API_KEY}, headers={'Content-Type': 'application/json'}, files={'file':('request.json', open('request.json', 'rb'))})
+payload = open("request.json")
+
+vision_request = requests.post(url=API_VISION, params={'key': API_KEY}, headers={'Content-Type': 'application/json'}, data=payload)
 translate_request = requests.post(url=API_TRANSLATE, data={'key': API_KEY, 'source': 'en', 'target': 'es', 'q': 'Hello World'})
 
 print(vision_request.text)
