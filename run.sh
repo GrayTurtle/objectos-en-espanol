6ab095ff1cf6e4cfb057cad71869f843d2bc8fab
Jason Nguyen <Jason@Jasons-MacBook-Air.local>
2018-09-12 13:17:05 -0500

    created bash schript

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./objectos-en-espanol.py cats.jpg
+./objectos-en-espanol.py cat.jpg
+./objectos-en-espanol.py cats.pdf
+
+./objectos-en-espanol.py dogs.jpg
+./objectos-en-espanol.py water_bottle.jpg
+./objectos-en-espanol.py car.jpg
+./objectos-en-espanol.py backpack.jpg
+./objectos-en-espanol.py Burger.jpg
+
